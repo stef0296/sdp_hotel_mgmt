8cae452be0e79a92cbfc26e77b53593971efe1f1
stef0296 <steff.alvares@gmail.com>
2021-06-06 21:30:27 +0200

    added checkin functionality

--- five-star-hotel/checkin.js
+++ five-star-hotel/checkin.js
@@ -1,40 +1,62 @@
 
-  function Checkin() {
+async function checkin() {
+  let bookingId = document.getElementById('book_id').value;
 
-    var bookigid = document.querySelector("#book_id").value;
-    var bookingdate = document.querySelector('#checkin_date').value;
-    console.log(bookigid);
-    console.log(bookingdate);
+  let procName = 'spGetBookingDetails';
+  let procParams = {
+    'book_id': bookingId
   }
 
-  function CleanCheck() {
-    var cleaned = document.getElementById("Clean").checked;
+  let data = await getData(procName, procParams);
+  console.log(data);
 
-    if (cleaned == true) {
-      cleaned = 1;
-    }
-    else {
-      cleaned = 0;
-    }
-    console.log(cleaned);
-    var fridge = document.getElementById("Fridge").checked;
-    if (fridge == true) {
-      fridge = 1;
-    }
-    else {
-      fridge = 0;
-    }
+  document.getElementById('fname').value = data[0][0].book_cust_fName;
+  document.getElementById('lname').value = data[0][0].book_cust_lName;
+  document.getElementById('address').value = data[0][0].book_cust_address;
+  document.getElementById('email').value = data[0][0].book_cust_email;
+  document.getElementById('phone_no').value = data[0][0].book_cust_ph_num;
 
-    console.log(fridge);
-    var fridge = document.getElementById("Maintain").checked;
-    if (fridge == true) {
-      fridge = 1;
-    }
-    else {
-      fridge = 0;
-    }
-    console.log(fridge);
+  let table = document.getElementById('room_table').getElementsByTagName('tbody')[0];
 
+  for (let room of data[1]) {
+    // Insert a row at the end of table
+    let newRow = table.insertRow();
+    let keys = Object.keys(room);
+
+    keys.forEach((key, idx) => {
+      createTableCell(newRow, room[`${key}`]);
+    });
   }
+}
 
+function createTableCell(newRow, nodeText) {
+  // Insert a cell at the end of the row
+  let newCell = newRow.insertCell();
 
+  // Append a text node to the cell
+  let newText = document.createTextNode(nodeText);
+  newCell.appendChild(newText);
+}
+
+async function processCheckin() {
+  let bookId = document.getElementById('book_id').value;
+  let checkinDate = document.getElementById('checkin_date').value;
+  checkinDate = `${checkinDate.substr(8, 2)}-${checkinDate.substr(5, 2)}-${checkinDate.substr(0, 4)}`;
+  let cleanStatus = document.getElementById('clean_status').checked;
+  let fridgeStatus = document.getElementById('fridge_status').checked;
+  let maintStatus = document.getElementById('maint_status').checked;
+
+  let procName = 'spNewCheckin';
+  let procParams = {
+    'book_id': bookId,
+    'checkin_date': checkinDate,
+    'cin_clean_status': cleanStatus,
+    'cin_fridge_status': fridgeStatus,
+    'cin_maint_status': maintStatus
+  };
+
+  let result = await postData(procName, procParams);
+  if(result[0][0].result_id == '1') {
+    alert(`Customer checked in successfully`);
+}
+}
